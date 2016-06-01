35ecd541f4d47c645b4af5839795cf8bb94c4ade
Daniel Fernandez <dtfern@gmail.com>
2016-06-01 22:52:09 +0100

    Added stutter type

--- ghostType.js
+++ ghostType.js
@@ -1,87 +1,88 @@
-(function($){
-	$.fn.GhostType = function(options){
-		var settings = $.extend({
-			wordArray: ["Lorem Ipsum", "Foo bar", "Ghost Type"],
-			timeInterval: 100,
-			showWordTime: 3,
-			hideWordTime: 1,
-			startMode: 4,
-			cursor: true,
-			cursorChar: "|",
-			cursorTime: 0.5,
-			stutterType: false
-		}, options);
+(function ($) {
+    $.fn.GhostType = function (options) {
+        var settings = $.extend({
+            wordArray: ["Lorem Ipsum", "Foo bar", "Ghost Type"],
+            timeInterval: 100,
+            showWordTime: 3,
+            hideWordTime: 1,
+            startMode: 4,
+            cursor: true,
+            cursorChar: "|",
+            cursorTime: 0.5,
+            stutterType: false,
+            stutterProbability: 0
+        }, options);
 
-		return this.each(function(){
-			var mode = settings.startMode;
-			var showTimeInterval = (settings.showWordTime*1000)/settings.timeInterval;
-			var hideTimeInterval = (settings.hideWordTime*1000)/settings.timeInterval;
-			var cursorInterval = (settings.cursorTime*1000)/settings.timeInterval;
-			var keyWords = settings.wordArray;
-			var cursorShow = settings.cursor;
-			var intervalCount = 0;
-			var wordIndex = 0;
-			var letterIndex = 0;
-			var cursorCount = 0;
-			var stutterWrite = settings.stutterType;
-			$(this).append("<span id=\"gtText\"></span>");
-			if(cursorShow){
-				$(this).append("<span id=\"gtCursor\">" + settings.cursorChar + "</span>");
-			}
+        return this.each(function () {
+            var mode = settings.startMode;
+            var showTimeInterval = (settings.showWordTime * 1000) / settings.timeInterval;
+            var hideTimeInterval = (settings.hideWordTime * 1000) / settings.timeInterval;
+            var cursorInterval = (settings.cursorTime * 1000) / settings.timeInterval;
+            var keyWords = settings.wordArray;
+            var cursorShow = settings.cursor;
+            var intervalCount = 0;
+            var wordIndex = 0;
+            var letterIndex = 0;
+            var cursorCount = 0;
+            var stutterWrite = settings.stutterType;
+            var stutterProbability = settings.stutterProbability;
+            $(this).append("<span id=\"gtText\"></span>");
+            if (cursorShow) {
+                $(this).append("<span id=\"gtCursor\">" + settings.cursorChar + "</span>");
+            }
 
-			setInterval($.proxy(ghoster, this), settings.timeInterval);
+            setInterval($.proxy(ghoster, this), settings.timeInterval);
 
-			function ghoster(){
-				var inWord=$("#gtText").text();
-				var currWord=keyWords[wordIndex];
-				var inWordLength=inWord.length;
-				if(cursorShow){
-					cursorCount++;
-					if(cursorCount==cursorInterval){
-						if($("#gtCursor").is(":visible")){
-							$("#gtCursor").hide();
-						}else{
-							$("#gtCursor").show();
-						}
-						cursorCount = 0;
-					}
-				}
-				if(mode==1){
-					var doAction = true;
-					
-					letterIndex++;
-					$("#gtText").text(currWord.substring(0,letterIndex));
-					if(letterIndex==currWord.length){
-						mode++;
-						letterIndex=inWordLength+1;
-						wordIndex++;
-						if(wordIndex==keyWords.length){
-							wordIndex=0;
-						}
-					}
-				}
-				else if(mode==2){
-					intervalCount++;
-					if(intervalCount==showTimeInterval){
-						mode++;
-						intervalCount=0;
-					}
-				}
-				else if(mode==3){
-					letterIndex--;
-					$("#gtText").text(inWord.substring(0,letterIndex));
-					if(letterIndex==0){
-						mode++;
-					}
-				}
-				else if(mode==4){
-					intervalCount++;
-					if(intervalCount==hideTimeInterval){
-						mode=1;
-						intervalCount=0;
-					}
-				}
-			}
-		});
-	};
+            function ghoster() {
+                var inWord = $("#gtText").text();
+                var currWord = keyWords[wordIndex];
+                var inWordLength = inWord.length;
+                var stutter = (stutterWrite && !(Math.random() < stutterProbability) || !stutterWrite);
+                if (cursorShow) {
+                    cursorCount++;
+                    if (cursorCount == cursorInterval) {
+                        if ($("#gtCursor").is(":visible")) {
+                            $("#gtCursor").hide();
+                        } else {
+                            $("#gtCursor").show();
+                        }
+                        cursorCount = 0;
+                    }
+                }
+                if (mode == 1 && stutter) {
+                    letterIndex++;
+                    $("#gtText").text(currWord.substring(0, letterIndex));
+                    if (letterIndex == currWord.length) {
+                        mode++;
+                        letterIndex = inWordLength + 1;
+                        wordIndex++;
+                        if (wordIndex == keyWords.length) {
+                            wordIndex = 0;
+                        }
+                    }
+                }
+                else if (mode == 2) {
+                    intervalCount++;
+                    if (intervalCount == showTimeInterval) {
+                        mode++;
+                        intervalCount = 0;
+                    }
+                }
+                else if (mode == 3) {
+                    letterIndex--;
+                    $("#gtText").text(inWord.substring(0, letterIndex));
+                    if (letterIndex == 0) {
+                        mode++;
+                    }
+                }
+                else if (mode == 4) {
+                    intervalCount++;
+                    if (intervalCount == hideTimeInterval) {
+                        mode = 1;
+                        intervalCount = 0;
+                    }
+                }
+            }
+        });
+    };
 }(jQuery));
